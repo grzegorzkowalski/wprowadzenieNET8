13c6f0bfbdc183631362a034b49a3da64a527939
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-01-19 08:58:12 +0100

    add film controller

---
+++ FilmDB/Controllers/FilmController.cs
@@ -0,0 +1,40 @@
+﻿using FilmDB.Models;
+using FilmDB.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDB.Controllers
+{
+    public class FilmController : Controller
+    {
+        private readonly FilmManager _filmManager;
+        public FilmController(FilmManager filmManager) 
+        { 
+            _filmManager = filmManager;
+        }
+        public IActionResult Index()
+        {
+            var films = _filmManager.GetFilms();
+            return View(films);
+        }
+
+        [HttpGet]
+        public IActionResult AddFilm() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddFilm(Film film)
+        {
+            try
+            {
+                _filmManager.AddFilm(film);
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View(film);
+            }             
+        }
+    }
+}
--- FilmDB/Models/Film.cs
+++ FilmDB/Models/Film.cs
@@ -1,9 +1,13 @@
-﻿namespace FilmDB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmDB.Models
 {
     public class Film
     {
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Title { get; set; }
+        [Range(1890,2200)]
         public int Year { get; set; }
     }
 }
